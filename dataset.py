23e17eddb473ccc87c95644fd1078e238f0a1531
davidlainesv <david.dev@outlook.com>
2023-02-02 16:16:40 -0600

    - dataset accepts normalization - augmentation accepts any list

--- dataset.py
+++ dataset.py
@@ -1,12 +1,11 @@
-import pandas as pd
 import tensorflow as tf
-from sklearn.model_selection import StratifiedKFold
-from config import INPUT_HEIGHT, INPUT_WIDTH, RANDOM_SEED
+from config import INPUT_HEIGHT, INPUT_WIDTH
 from data_augmentation import RandomFlip, RandomScale, RandomShift, RandomRotation, RandomSpeed
 from preprocessing import PadIfLessThan, ResizeIfMoreThan, preprocess_dataframe
 from skeleton_graph import tssi_v2
 from sklearn.preprocessing import OneHotEncoder
 import numpy as np
+from core import Normalization
 
 
 available_augmentations = {
@@ -113,26 +112,11 @@ class Dataset():
         # retrieve the joints order
         _, _, joints_order = tssi_v2()
 
-        # preprocess the train dataframe
-        train_dataframe = preprocess_dataframe(train_dataframe,
-                                               with_root=True,
-                                               with_midhip=False)
-
-        # preprocess the validation dataframe
-        val_dataframe = preprocess_dataframe(validation_dataframe,
-                                             with_root=True,
-                                             with_midhip=False)
-
         # obtain characteristics of the dataset
         num_train_examples = len(train_dataframe["video"].unique())
-        num_val_examples = len(val_dataframe["video"].unique())
+        num_val_examples = len(validation_dataframe["video"].unique())
         num_total_examples = num_train_examples + num_val_examples
-
-        # preprocess the test dataframe
         if test_dataframe is not None:
-            test_dataframe = preprocess_dataframe(test_dataframe,
-                                                  with_root=True,
-                                                  with_midhip=False)
             num_test_examples = len(test_dataframe["video"].unique())
         else:
             num_test_examples = 0
@@ -140,19 +124,27 @@ class Dataset():
         # expose variables
         self.joints_order = joints_order
         self.train_dataframe = train_dataframe
-        self.val_dataframe = val_dataframe
+        self.validation_dataframe = validation_dataframe
         self.test_dataframe = test_dataframe
         self.num_train_examples = num_train_examples
         self.num_val_examples = num_val_examples
         self.num_test_examples = num_test_examples
         self.num_total_examples = num_total_examples
 
-    def get_training_set(self, batch_size=32,
-                         buffer_size=5000, repeat=False,
-                         deterministic=False, augmentations=None):
-        # define length_normalization layers
-        # NOTE: if applied, random speed augmentation
-        # changes the length of the samples a priori
+    def get_training_set(self,
+                         batch_size=32,
+                         buffer_size=5000,
+                         repeat=False,
+                         deterministic=False,
+                         augmentations=[],
+                         normalization=Normalization.Neg1To1):
+        # preprocess the train dataframe
+        train_dataframe = preprocess_dataframe(self.train_dataframe,
+                                               with_root=True,
+                                               with_midhip=False,
+                                               normalization=normalization)
+
+        # define the length_normalization layers
         train_length_normalization = tf.keras.Sequential([
             PadIfLessThan(frames=INPUT_HEIGHT),
             ResizeIfMoreThan(frames=INPUT_HEIGHT)
@@ -163,9 +155,6 @@ class Dataset():
         if augmentations == "all":
             augmentations = augmentations_order
 
-        if augmentations == None:
-            augmentations = []
-
         # define the augmentation layers
         # based on the list of augmentations
         layers = [available_augmentations[aug] for aug in augmentations]
@@ -175,13 +164,12 @@ class Dataset():
         @tf.function
         def train_map_fn(x, y):
             batch = tf.expand_dims(x, axis=0)
-            # batch = train_preprocessing(batch)
             batch = train_augmentation(batch, training=True)
             x = train_length_normalization(batch)[0]
             x = tf.ensure_shape(x, [INPUT_HEIGHT, INPUT_WIDTH, 3])
             return x, y
 
-        dataset = generate_train_dataset(self.train_dataframe,
+        dataset = generate_train_dataset(train_dataframe,
                                          self.joints_order,
                                          [],
                                          train_map_fn,
@@ -192,11 +180,18 @@ class Dataset():
 
         return dataset
 
-    def get_validation_set(self, batch_size=32):
+    def get_validation_set(self,
+                           batch_size=32,
+                           normalization=Normalization.neg1to1):
+        # preprocess the validation dataframe
+        val_dataframe = preprocess_dataframe(self.validation_dataframe,
+                                             with_root=True,
+                                             with_midhip=False,
+                                             normalization=normalization)
+
         # define the preprocessing
         # for the test dataset
         val_preprocessing = tf.keras.Sequential([
-            # tf.keras.layers.Rescaling(scale=255.0, offset=0.0),
             PadIfLessThan(frames=INPUT_HEIGHT)
         ], name="preprocessing")
 
@@ -207,7 +202,7 @@ class Dataset():
             x = val_preprocessing(x)
             return x, y
 
-        dataset = generate_test_dataset(self.val_dataframe,
+        dataset = generate_test_dataset(val_dataframe,
                                         self.joints_order,
                                         [],
                                         val_map_fn,
@@ -215,14 +210,23 @@ class Dataset():
 
         return dataset
 
-    def get_testing_set(self, batch_size=32):
+    def get_testing_set(self,
+                        batch_size=32,
+                        normalization=Normalization.Neg1To1):
+        # raise exception if test_dataframe
+        # does not exist
         if self.test_dataframe is None:
-            return None
+            raise Exception("Test dataframe was not provided")
+
+        # preprocess the test dataframe
+        test_dataframe = preprocess_dataframe(self.test_dataframe,
+                                              with_root=True,
+                                              with_midhip=False,
+                                              normalization=normalization)
 
         # define the preprocessing
         # for the test dataset
         test_preprocessing = tf.keras.Sequential([
-            # tf.keras.layers.Rescaling(scale=255.0, offset=0.0),
             PadIfLessThan(frames=INPUT_HEIGHT)
         ], name="preprocessing")
 
@@ -233,7 +237,7 @@ class Dataset():
             x = test_preprocessing(x)
             return x, y
 
-        dataset = generate_test_dataset(self.test_dataframe,
+        dataset = generate_test_dataset(test_dataframe,
                                         self.joints_order,
                                         [],
                                         test_map_fn,
