d88281d5deb23de32307a51b68854ea47b35bc13
davidlainesv <david.dev@outlook.com>
2023-02-09 23:13:58 -0600

    fixed translation scale invariant

--- preprocessing.py
+++ preprocessing.py
@@ -222,68 +222,92 @@ class Center(tf.keras.layers.Layer):
 
 
 class TranslationScaleInvariant(tf.keras.layers.Layer):
-    def __init__(self, level='channel', **kwargs):
+    def __init__(self, level='frame', **kwargs):
         super().__init__(**kwargs)
         self.level_dict = {
-            'channel': tf.constant(0),
+            'frame': tf.constant(0),
             'joint': tf.constant(1)
         }
         self.level = self.level_dict[level]
 
     @tf.function
-    def channel_level_translation_scale_invariant(self, batch):
+    def frame_level(self, batch):
         # batch.shape => (examples, frames, joints, coordinates)
         # [color].shape => (examples, frames, joints)
         [red, green, blue] = tf.unstack(batch, axis=-1)
 
-        # [color]_min.shape => (examples, 1, 1)
-        red_min = tf.reduce_min(red, axis=[-1, -2], keepdims=True)
-        green_min = tf.reduce_min(green, axis=[-1, -2], keepdims=True)
-        blue_min = tf.reduce_min(blue, axis=[-1, -2], keepdims=True)
+        # [color]_min.shape => (examples, frames, 1)
+        # min at each frame per channel
+        red_min = tf.reduce_min(red, axis=-1, keepdims=True)
+        green_min = tf.reduce_min(green, axis=-1, keepdims=True)
+        blue_min = tf.reduce_min(blue, axis=-1, keepdims=True)
 
-        # [color]_max.shape => (examples, 1, 1)
-        red_max = tf.reduce_max(red, axis=[-1, -2], keepdims=True)
-        green_max = tf.reduce_max(green, axis=[-1, -2], keepdims=True)
-        blue_max = tf.reduce_max(blue, axis=[-1, -2], keepdims=True)
+        # [color]_max.shape => (examples, frames, 1)
+        # max at each frame per channel
+        red_max = tf.reduce_max(red, axis=-2, keepdims=True)
+        green_max = tf.reduce_max(green, axis=-2, keepdims=True)
+        blue_max = tf.reduce_max(blue, axis=-2, keepdims=True)
+
+        # [color]_dist.shape => (examples, frames, 1)
+        # distance between max and min at each frame per channel
+        red_dist = red_max - red_min
+        green_dist = green_max - green_min
+        blue_dist = blue_max - blue_min
+
+        # [color]_dist_max.shape => ()
+        # max_distance of all frames per channel
+        red_dist_max = tf.reduce_max(red_dist)
+        green_dist_max = tf.reduce_max(green_dist)
+        blue_dist_max = tf.reduce_max(blue_dist)
 
         # new_[color].shape => (examples, frames, joints)
-        new_red = tf.math.divide_no_nan((red - red_min), (red_max - red_min))
-        new_green = tf.math.divide_no_nan(
-            (green - green_min), (green_max - green_min))
-        new_blue = tf.math.divide_no_nan(
-            (blue - blue_min), (blue_max - blue_min))
+        new_red = tf.math.divide_no_nan((red - red_min), red_dist_max)
+        new_green = tf.math.divide_no_nan((green - green_min), green_dist_max)
+        new_blue = tf.math.divide_no_nan((blue - blue_min), blue_dist_max)
 
         return tf.stack([new_red, new_green, new_blue], axis=-1)
 
     @tf.function
-    def joint_level_translation_scale_invariant(self, batch):
+    def joint_level(self, batch):
         # batch.shape => (examples, frames, joints, coordinates)
         # [color].shape => (examples, frames, joints)
         [red, green, blue] = tf.unstack(batch, axis=-1)
 
         # [color]_min.shape => (examples, 1, joints)
+        # min at each joint per channel
         red_min = tf.reduce_min(red, axis=-2, keepdims=True)
         green_min = tf.reduce_min(green, axis=-2, keepdims=True)
         blue_min = tf.reduce_min(blue, axis=-2, keepdims=True)
 
         # [color]_max.shape => (examples, 1, joints)
+        # max at each joint per channel
         red_max = tf.reduce_max(red, axis=-2, keepdims=True)
         green_max = tf.reduce_max(green, axis=-2, keepdims=True)
         blue_max = tf.reduce_max(blue, axis=-2, keepdims=True)
 
+        # [color]_dist.shape => (examples, 1, joint)
+        # distance between max and min at each joint per channel
+        red_dist = red_max - red_min
+        green_dist = green_max - green_min
+        blue_dist = blue_max - blue_min
+
+        # [color]_dist_max.shape => ()
+        # max_distance of all joints per channel
+        red_dist_max = tf.reduce_max(red_dist)
+        green_dist_max = tf.reduce_max(green_dist)
+        blue_dist_max = tf.reduce_max(blue_dist)
+
         # new_[color].shape => (examples, frames, joints)
-        new_red = tf.math.divide_no_nan((red - red_min), (red_max - red_min))
-        new_green = tf.math.divide_no_nan(
-            (green - green_min), (green_max - green_min))
-        new_blue = tf.math.divide_no_nan(
-            (blue - blue_min), (blue_max - blue_min))
+        new_red = tf.math.divide_no_nan((red - red_min), red_dist_max)
+        new_green = tf.math.divide_no_nan((green - green_min), green_dist_max)
+        new_blue = tf.math.divide_no_nan((blue - blue_min), blue_dist_max)
 
         return tf.stack([new_red, new_green, new_blue], axis=-1)
 
     @tf.function
     def call(self, batch):
         batch = tf.cond(
-            self.level == self.level_dict['channel'],
-            lambda: self.channel_level_translation_scale_invariant(batch),
-            lambda: self.joint_level_translation_scale_invariant(batch))
+            self.level == self.level_dict['frame'],
+            lambda: self.frame_level(batch),
+            lambda: self.joint_level(batch))
         return batch
