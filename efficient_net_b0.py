9a7e5ecc80c2189ee3bca4cb6fae8ca6e1e248b5
davidlainesv <david.dev@outlook.com>
2023-02-16 01:02:28 -0600

    fix error model expected 130 layers

--- efficient_net_b0.py
+++ efficient_net_b0.py
@@ -347,8 +347,7 @@ def EfficientNet(
     # Build stem
     x = img_input
     # x = layers.Rescaling(1.0 / 255.0)(x)
-    # x = layers.Normalization(axis=bn_axis)(x)
-    x = layers.Normalization(axis=bn_axis, mean=0., variance=1.)(x)
+    x = layers.Normalization(axis=bn_axis)(x)
     if weights == "imagenet":
         # Note that the normaliztion layer uses square value of STDDEV as the
         # variance for the layer: result = (input - mean) / sqrt(var)
